bd5365db4c20353d8b15181882958f6a2d45621f
SJinHoo <sjh11016@gmail.com>
2023-05-04 21:47:44 +0900

    초기 커밋.

---
+++ Project_R/Game.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_R
+{
+    public class Game
+    {
+        public bool running = true;
+
+        public void Run()
+        {   
+            //초기화
+            Init();
+
+            while (running)
+            {
+                Render();
+                Input();
+                Update();
+            }
+
+            Release();
+        }
+        
+        //초기화
+        public void Init()
+        {
+
+        }
+
+        public void Render()
+        {
+
+        }
+
+        public void Input()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Release()
+        {
+
+        }
+    }
+
+   
+}
---
+++ Project_R/Scene/MainMenuScene.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_R
+{
+    public class MainMenuScene : Scene
+    {
+        public MainMenuScene(Game game) : base(game)
+        {
+
+        }
+
+        public override void Render()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(" 1. 게임 시작 ");
+            sb.AppendLine(" 2. 게임 종료 ");
+            sb.Append(" ");
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        public override void Update()
+        {
+            string input = Console.ReadLine();
+
+            int index;
+            if(! int .TryParse(input, out index))
+            {
+                Console.WriteLine("잘 못된 입력입니다.");
+                Thread.Sleep(2000);
+                return;
+            }
+
+            switch (index)
+            {
+                case 1:
+                    
+            }
+        }
+    }
+}
